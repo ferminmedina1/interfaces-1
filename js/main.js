fe043b2b25437390b5b57711311f101927df0582
ArleoAgustin <arleo.agustin20@gmail.com>
2021-09-08 20:05:43 -0300

    adds 3 filters

--- js/main.js
+++ js/main.js
@@ -59,9 +59,9 @@ function loadPage () {
 
 //evento del slider de brillo
 
-    document.querySelector("#brightness").addEventListener("click", function(e) {
-       //hacer
-    });
+    // document.querySelector("#brightness").addEventListener("click", function(e) {
+    //    //hacer
+    // });
 
 
 //evento para limpiar el canvas
@@ -70,6 +70,7 @@ function loadPage () {
         canvas.width = 800;
         canvas.height = 500;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        accionarLapiz();
     });
 
 
@@ -106,61 +107,69 @@ function loadPage () {
     }
     
 
-/////////////////////////////////////////////////REVISAR///////////////////////////////////////////////////////  
+//evento para cargar la imagen
 
-//imagen precargada, arreglar para cuando se quiere cargar desde la pagina
-
-
-
-    let imgInput = document.getElementById('myFile');
-    imgInput.addEventListener('change', function(e) {
+    document.getElementById('myFile').addEventListener('change', function(e) {
+        
       if(e.target.files) {
         let imageFile = e.target.files[0]; //aca obtenemos el archivo
         var reader = new FileReader();
         reader.readAsDataURL(imageFile);
+
         reader.onloadend = function (e) {
           var myImage = new Image(); // Crea la imageb
           myImage.src = e.target.result; 
+
           myImage.onload = function() {
-              canvas.width = myImage.width; // Assigns image's width to canvas
-              canvas.height = myImage.height; // Assigns image's height to canvas
+            // myImage.width = canvas.width; // Assigns image's width to canvas
+            // myImage.height = canvas.height; // Assigns image's height to canvas
               ctx.drawImage(myImage, 0, 0);
+              habilitarFiltros()
           }
         }
       }
+      
     });
 
 
+//funcion para habilitar los filtros una vez subida la imagen
 
-//evento para seleccionar el filtro
+    function habilitarFiltros(){
 
-    document.getElementById("filter-select").addEventListener("click", function(){
+        let botones = document.querySelectorAll("button");  //se obtienen los botones
 
-    
-        let filtro = document.getElementById("filter-select").value;
+        for(let i=0; i < botones.length; i++) {   //se recorre el arreglo de botones
 
-        
-        if (filtro == "negativo"){   //si el filtro seleccionado es..
-            filterNegativo();   
+            botones[i].addEventListener("click", function(){    //si se hace click se crea el evento
+            
+                if (botones[i].value == "negativo"){   //si el filtro seleccionado es..
+                    filterNegativo();   
+                }
+
+                if (botones[i].value == "brillo"){
+                    filterBrillo();
+                } 
+
+                if (botones[i].value == "saturacion"){
+                    filterSaturacion();
+                }
+
+                if (botones[i].value == "binarizacion"){
+                    filterBinarizacion()
+                }
+
+                if (botones[i].value == "sepia"){
+                    //hacer
+                } 
+
+                if (botones[i].value == "blur"){
+                    //hacer
+                }
+            
+            })
+
         }
-
-        if (filtro == "saturacion"){
-            //hacer
-        } 
-
-        if (filtro == "sepia"){
-            //hacer
-        } 
-
-        if (filtro == "blur"){
-            //hacer
-        }
-
-        if (filtro == "binarizacion"){
-            //hacer
-        }
-        document.getElementById("filter-select").selectedIndex = 0;
-    })
+    }
 
 
  //funcion para el filtro negativo
@@ -185,6 +194,162 @@ function loadPage () {
         }
         ctx.putImageData(imageData, 0, 0);
         
+    }
+
+
+//funcion de para filtro de saturacion
+
+    function filterSaturacion(){
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        for (let x = 0; x < canvas.width; x++) {
+            for (let y = 0; y < canvas.height; y++) {
+                
+                let r = getRed(imageData, x, y);
+                let g = getGreen(imageData, x, y);
+                let b = getBlue(imageData, x, y);
+                let a = 255;
+                let hsv = rgbToHsv (r, g, b);
+                hsv.s += 3;
+                let jsonRGB = HSVtoRGB(hsv)
+                r = jsonRGB.r
+                g = jsonRGB.g
+                b = jsonRGB.b
+                setPixel(imageData, x, y, r, g, b, a);
+            }
+        }
+        ctx.putImageData(imageData, 0, 0);
+
+    }
+
+
+//funcion para filtro de brillo
+
+    function filterBrillo(){
+
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        for (let x = 0; x < canvas.width; x++) {
+            for (let y = 0; y < canvas.height; y++) {
+                
+                let r = getRed(imageData, x, y);
+                let g = getGreen(imageData, x, y);
+                let b = getBlue(imageData, x, y);
+                let a = 255;
+                let hsv = rgbToHsv (r, g, b);
+                hsv.v += 3;
+                let jsonRGB = HSVtoRGB(hsv)
+                r = jsonRGB.r
+                g = jsonRGB.g
+                b = jsonRGB.b
+                setPixel(imageData, x, y, r, g, b, a);
+            }
+        }
+        ctx.putImageData(imageData, 0, 0);
+
+    }
+
+
+//funcion para filtro de binarizacion
+
+    function filterBinarizacion(){
+
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        for (let x = 0; x < canvas.width; x++) {
+            for (let y = 0; y < canvas.height; y++) {
+                
+                let r = getRed(imageData, x, y);
+                let g = getGreen(imageData, x, y);
+                let b = getBlue(imageData, x, y);
+                let a = 255;
+                let gray =  (0.299 * r + 0.587 * g + 0.114 * b)
+                if ( gray > 120 ){
+                    r = 255;
+                    g = 255;
+                    b = 255;
+                }
+                else{
+                    r = 0;
+                    g = 0;
+                    b = 0;
+                }
+                setPixel(imageData, x, y, r, g, b, a);
+            }
+        }
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+//funcion para pasar de rgb a hsb
+
+    function rgbToHsv (r, g, b) {
+
+        let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+        rabs = r / 255;
+        gabs = g / 255;
+        babs = b / 255;
+        v = Math.max(rabs, gabs, babs),
+        diff = v - Math.min(rabs, gabs, babs);
+        diffc = c => (v - c) / 6 / diff + 1 / 2;
+        percentRoundFn = num => Math.round(num * 100) / 100;
+        if (diff == 0) {
+            h = s = 0;
+        } else {
+            s = diff / v;
+            rr = diffc(rabs);
+            gg = diffc(gabs);
+            bb = diffc(babs);
+
+            if (rabs === v) {
+                h = bb - gg;
+            } else if (gabs === v) {
+                h = (1 / 3) + rr - bb;
+            } else if (babs === v) {
+                h = (2 / 3) + gg - rr;
+            }
+            if (h < 0) {
+                h += 1;
+            }else if (h > 1) {
+                h -= 1;
+            }
+        }
+        return {
+            h: Math.round(h * 360),
+            s: percentRoundFn(s * 100),
+            v: percentRoundFn(v * 100)
+        };
+    }
+
+
+//funcion para pasar de HSV a RGB
+
+    function HSVtoRGB(hsv) {
+
+    var rgb = { };
+    var h = Math.round(hsv.h);
+    var s = Math.round(hsv.s * 255 / 100);
+    var v = Math.round(hsv.v * 255 / 100);
+
+        if (s == 0) {
+
+        rgb.r = rgb.g = rgb.b = v;
+        } else {
+        var t1 = v;
+        var t2 = (255 - s) * v / 255;
+        var t3 = (t1 - t2) * (h % 60) / 60;
+
+            if (h == 360) h = 0;
+
+                if (h < 60) { rgb.r = t1; rgb.b = t2; rgb.g = t2 + t3 }
+                else if (h < 120) { rgb.g = t1; rgb.b = t2; rgb.r = t1 - t3 }
+                else if (h < 180) { rgb.g = t1; rgb.r = t2; rgb.b = t2 + t3 }
+                else if (h < 240) { rgb.b = t1; rgb.r = t2; rgb.g = t1 - t3 }
+                else if (h < 300) { rgb.b = t1; rgb.g = t2; rgb.r = t2 + t3 }
+                else if (h < 360) { rgb.r = t1; rgb.g = t2; rgb.b = t1 - t3 }
+                else { rgb.r = 0; rgb.g = 0; rgb.b = 0 }
+        }
+
+    return { r: Math.round(rgb.r), g: Math.round(rgb.g), b: Math.round(rgb.b) };
     }
 
 
